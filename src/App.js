de34636e5a639d811b137a7ee0a275cbec2ee75f
Pubudu Arosha <it18108514@my.sliit.lk>
2020-08-18 11:18:46 +0530

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import Header from './components/Header.js'
-import Home from './pages/home/Home'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./pages/home/Home";
+import Faq from "./pages/footer/faq/Faq";
+import TermsAndConditions from "./pages/footer/termsAndConditions/TermsAndConditions";
+import Cart from "./pages/cart/Cart";
+import Grocery from "./pages/categories/grocery/Grocery";
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-
   let [loaded, setload] = useState(false);
 
-  useEffect(() => {
-    
-   
-  })
+  useEffect(() => {});
 
   return (
-         
-  
     <div className="App">
- <Header></Header>
-<Home/>
+      <Router>
+        <Header/>
+        <Switch>
+          <Route path={"/"} exact component={Home} />
+          <Route path={"/faq"} component={Faq} />
+          <Route
+            path={"/terms-and-conditions"}
+            component={TermsAndConditions}
+          />
+          <Route path={"/cart"} component={Cart} />
+          <Route path={"/categories-grocery"} component={Grocery} />
+          <Route path={"/*"} exact component={Home} />
+        </Switch>
+      </Router>
     </div>
-
   );
 }
 
