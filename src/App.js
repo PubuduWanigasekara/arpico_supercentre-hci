a09d448887451a9a0b78c2f50ffdadd56b6a75fc
Esandhavithanage <esandhavithanage@gmail.com>
2020-10-03 20:52:34 +0530

    store_locations added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer"
 import Home from "./pages/home/Home";
-import wishlist from "./pages/wishlist/wishlist";
+import Wishlist from "./pages/wishlist/Wishlist";
 import Faq from "./pages/footer/faq/Faq";
 import TermsAndConditions from "./pages/footer/termsAndConditions/TermsAndConditions";
+import store_locations from "./pages/store_locations/store_locations";
 import Cart from "./pages/cart/Cart";
 import Grocery from "./pages/categories/grocery/Grocery";
-import Login from "./pages/Login"
+import Login from "./pages/login/Login"
 import Baby from './pages/babyNeeds/Baby'
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
@@ -30,10 +31,11 @@ function App() {
             path={"/terms-and-conditions"}
             component={TermsAndConditions}
           />
-          <Route path={"/wishlist"} component={wishlist} />
+          <Route path={"/wishlist"} component={Wishlist} />
           <Route path={"/cart"} component={Cart} />
           <Route path={"/categories-grocery"} component={Grocery} />
           <Route path={"/baby"} component={Baby} />
+          <Route path={"/store_locations"} component={store_locations} />
 
 
 
--- src/pages/wishlist/wishlist.jsx
+++ src/pages/wishlist/wishlist.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import WishlistProductCard from '../../components/WishlistProductCard'
 
-export default function wishlist() {
+export default function Wishlist() {
+
+    let [data, Setdata] = useState([])
+
+    useEffect(() => {
+
+        if (JSON.parse(localStorage.getItem('wishlistItems'))) {
+            console.log("1");
+            Setdata(JSON.parse(localStorage.getItem('wishlistItems')))
+        }
+        else {
+            console.log("0");
+        }
+
+    }, []);
+
+    let sdata = {
+        name: "test",
+        price: 300,
+        qty: 2
+    }
+
+    const add = () => {
+        Setdata([...data, sdata])
+
+        console.log(data);
+    }
+   
+
+    const remove = (itm)=>{     
+        data.splice(itm,1)
+    }
+
     return (
         <div>
-            ddddddddddddfg
+
+            {data.map((it , index) => {
+                return (
+                    <WishlistProductCard
+                        removeItem={()=>remove}
+                        key={index}
+                        item={index}
+                        name={it.name}
+                        price={it.price}
+                        qty={it.qty} />
+                )
+            }
+            )}
+
+
+
+            <button onClick={add}>test</button>
         </div>
     )
 }
