9c3d29bca4efafaf82d484318fbe91c73f2ab867
Pubudu Arosha <it18108514@my.sliit.lk>
2020-10-15 12:58:43 +0530

    mycart page  updated

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,31 +1,29 @@
 import React, { useEffect, useState, useContext } from "react";
 import CartProductCard from "../../components/CartProductCard";
 import { AppContexts } from "../../contexts/AppContextsProvider";
-import {ToastContainer,toast} from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Cart() {
   //   cart
   let [total, Settotal] = useState(0);
-  const {cartitems, setcartitems} = useContext(AppContexts);
-  var t=0;
+  const { cartitems, setcartitems } = useContext(AppContexts);
+  var t = 0;
   useEffect(() => {
-    for(var i=0;i<cartitems.length;i++){
-      t = t+ cartitems[i].qty*cartitems[i].price
+    for (var i = 0; i < cartitems.length; i++) {
+      t = t + cartitems[i].qty * cartitems[i].price;
       console.log(t);
       Settotal(t);
       console.log(total);
     }
   }, []);
 
-
-
   let sdata = {
     id: 4,
     name: "test",
     price: 300,
     qty: 2,
-    isAddedToCart: true
+    isAddedToCart: true,
   };
 
   const add = () => {
@@ -34,18 +32,22 @@ export default function Cart() {
   };
 
   const remove = (id) => {
-    var message = '';
+    var message = "";
     console.log(id);
-    setcartitems(cartitems.filter(item => {
-      if(item.id != id){
-        return item;
-      }else{
-        message = item.name;
-        var subTot = total-(item.qty*item.price);
-        Settotal(subTot)
-      }
-    }));
-    toast.error("You removed "+message+" from wish list",{position: toast.POSITION.BOTTOM_RIGHT});
+    setcartitems(
+      cartitems.filter((item) => {
+        if (item.id != id) {
+          return item;
+        } else {
+          message = item.name;
+          var subTot = total - item.qty * item.price;
+          Settotal(subTot);
+        }
+      })
+    );
+    toast.error("You removed " + message + " from wish list", {
+      position: toast.POSITION.BOTTOM_RIGHT,
+    });
   };
 
   return (
@@ -54,7 +56,7 @@ export default function Cart() {
         <h3 id="cart_ttitle">Shopping Cart</h3>
         <div id="cartmaindiv">
           {/* left */}
-          <div id="cart_left_div">
+          <div id="cart_right_div">
             <div>
               <h5 id="cart_subtitle">Shipping Address</h5>
 
@@ -73,6 +75,26 @@ export default function Cart() {
               </form>
             </div>
             <div>
+              <h5 id="cart_subtitle">Order Summary</h5>
+              <form>
+                <label id="cart_label">Sub Total </label>
+                <label id="cart_input">LKR {total}</label>
+
+                <label id="cart_label">Shipping Fee </label>
+                <label id="cart_input">LKR 375</label>
+
+                <input id="cart_input" type="text" placeholder="Enter Discount Code" />
+                <button className="button primary cart_button">
+                  Apply 
+                </button>
+                <hr/>
+
+                <label id="cart_label">ORDER TOTAL </label>
+                <label id="cart_input">LKR {total + 375}</label>
+              </form>
+            </div>
+            <br />
+            <div>
               <h5 id="cart_subtitle">Shipping Methods</h5>
               <div id="cartbuttonsdiv">
                 <button className="button primary cart_button">
@@ -89,8 +111,8 @@ export default function Cart() {
             </div>
           </div>
           {/* right */}
-          <div id="cart_right_div">
-            <h5 id="cart_subtitle">Order Summary</h5>
+          <div id="cart_left_div">
+            <h5 id="cart_subtitle">My Cart</h5>
             <div id="c_items_div">
               {cartitems.map((it, index) => {
                 return (
